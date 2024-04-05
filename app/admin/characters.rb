d29f7540c3875c497406a2a004fe41c5b34ac194
obafunsho <oadelusi@academic.rrc.ca>
2024-04-05 15:44:42 -0500

    active admin: register models

---
+++ app/admin/characters.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Character do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :price, :stat_combat, :stat_durability, :stat_intelligence, :stat_power, :stat_speed, :stat_strength, :publisher_id, :alignment_id, :race_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :price, :stat_combat, :stat_durability, :stat_intelligence, :stat_power, :stat_speed, :stat_strength, :publisher_id, :alignment_id, :race_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
