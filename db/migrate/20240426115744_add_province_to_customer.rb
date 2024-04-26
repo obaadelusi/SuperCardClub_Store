cd1384858c798f564f6a74bebd5a6f021f174346
obafunsho <oadelusi@academic.rrc.ca>
2024-04-26 08:38:16 -0500

    province nullable

--- db/migrate/20240426115744_add_province_to_customer.rb
+++ db/migrate/20240426115744_add_province_to_customer.rb
@@ -1,5 +1,5 @@
 class AddProvinceToCustomer < ActiveRecord::Migration[7.1]
   def change
-    add_reference :customers, :province, null: false, foreign_key: true
+    add_reference :customers, :province, null: true, foreign_key: true
   end
 end
